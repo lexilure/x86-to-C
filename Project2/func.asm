3db52fe38ce50c190be7a4219307b06873861955
lexilure <alexandrapadua01@gmail.com>
2024-04-02 22:53:38 +0800

    main

--- Project2/func.asm
+++ Project2/func.asm
@@ -3,21 +3,22 @@ global dot_product_asm
 bits 64
 
 dot_product_asm:
-    xor rsi, rsi           
-    xorpd xmm0, xmm0          
+    xor rsi, rsi             
+    xorpd xmm0, xmm0         
 
 loop_start:
-    cmp rsi, r8              
-    jge loop_end             
+    cmp rsi, r8           
+    jge loop_end           
 
+    ; Multiply vectors element-wise and accumulate
     movsd xmm1, [rcx + rsi*8] 
     movsd xmm2, [rdx + rsi*8] 
-    mulsd xmm1, xmm2          
-    addsd xmm0, xmm1          
+    mulsd xmm1, xmm2      
+    addsd xmm0, xmm1       
 
     inc rsi                  
-    jmp loop_start           
+    jmp loop_start            
 
 loop_end:
-    movsd xmm1, xmm0          
+    movsd xmm1, xmm0      
     ret
--- Project2/main.c
+++ Project2/main.c
@@ -3,7 +3,6 @@
 #include <math.h>
 #include <windows.h>
 
-
 extern double dot_product_asm(const double* a, const double* b, int n);
 
 double dot_product_c(const double* a, const double* b, int n) {
@@ -13,13 +12,15 @@ double dot_product_c(const double* a, const double* b, int n) {
     }
     return result;
 }
-int main() { 
+
+int main() {
     int n;
-    printf("Enter the size of the vectors (2^n): ");
+    printf("Enter the size of the vectors(2^n): ");
     scanf_s("%d", &n);
+
     int size = (int)pow(2, n);
+
  
-    //memory allocation
     double* a = (double*)malloc(size * sizeof(double));
     double* b = (double*)malloc(size * sizeof(double));
 
@@ -28,6 +29,7 @@ int main() {
         return 1;
     }
 
+   
     for (int i = 0; i < size; i++) {
         a[i] = i + 1;         
         b[i] = size - i;     
@@ -38,25 +40,25 @@ int main() {
 
     double time_c_total = 0.0, time_asm_total = 0.0, time_c = 0.0, time_asm = 0.0;
     double result_c, result_asm;
-    int iterations = 30; // how many times it will run
+    int iterations = 30; // Run 30 times
 
     for (int i = 0; i < iterations; i++) {
         QueryPerformanceCounter(&start);
-        result_c = dot_product_c(a, b, size); // passing size
+        result_c = dot_product_c(a, b, size); 
         QueryPerformanceCounter(&end);
-        time_c = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart; // acumulate time in miliseconds
+        time_c = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart; 
         time_c_total += time_c;
         printf("Dot product C: %lf, Time: %lf ms\n", result_c, time_c);
 
         QueryPerformanceCounter(&start);
-        result_asm = dot_product_asm(a, b, size); // passing size
+        result_asm = dot_product_asm(a, b, size); 
         QueryPerformanceCounter(&end);
-        time_asm = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart; // Accumulate time in milliseconds
+        time_asm = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart; 
         time_asm_total += time_asm;
         printf("Dot product asm: %lf, Time: %lf ms\n", result_asm, time_asm);
     }
 
-    // calculate average times
+
     double avg_time_c = time_c_total / iterations;
     double avg_time_asm = time_asm_total / iterations;
     if (result_c == result_asm)
@@ -66,12 +68,9 @@ int main() {
     printf("Average Dot product C time: %lf ms\n", avg_time_c);
     printf("Average Dot product ASM time: %lf ms\n", avg_time_asm);
 
-    // free allocated memory
+
     free(a);
     free(b);
 
     return 0;
-
-
-
-    }
+}
