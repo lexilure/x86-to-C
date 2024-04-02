633d9dd83599a95eefb96f47ab2982f397475371
lexilure <alexandrapadua01@gmail.com>
2024-04-02 18:16:54 +0800

    random redo

--- Project2/func.asm
+++ Project2/func.asm
@@ -1,29 +1,23 @@
 section .text
-bits 64
-default rel 
-
 global dot_product_asm
+bits 64
 
-dot_product_asm:              
-    xor rsi, rsi               ; Start index at 0
-    ; Initialize result to 0
-    xorpd xmm0, xmm0           ; xmm0 = 0.0 , accumulator for the sdot result
+dot_product_asm:
+    xor rsi, rsi           
+    xorpd xmm0, xmm0          
 
 loop_start:
-    cmp rsi, r8             ; Compare current index with n
-    jge loop_end               ; If rsi >= n, we're done
+    cmp rsi, r8              
+    jge loop_end             
 
-    ; Multiply vectors element-wise and accumulate
     movsd xmm1, [rcx + rsi*8] 
     movsd xmm2, [rdx + rsi*8] 
-    mulsd xmm1, xmm2         
+    mulsd xmm1, xmm2          
     addsd xmm0, xmm1          
 
-    inc rsi                
+    inc rsi                  
     jmp loop_start           
 
-
 loop_end:
-    ; Return the result
-    movq xmm1, xmm0 ; Move the result to rax
+    movsd xmm1, xmm0          
     ret
